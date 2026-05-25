b1cae98858f84df48418cfbc930641654671695a
dukaelko-ux <dukaelko@gmail.com>
2026-05-25 15:41:32 +0700

    Add konversi.py to demonstrate data structures

---
+++ konversi.py
@@ -0,0 +1,15 @@
+data_list = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+print('List:', data_list)
+print('Set :', set(data_list)) 
+
+hewan_set = {'kucing', 'anjing', 'kelinci'}
+print('Set :', hewan_set)
+print('List:', list(hewan_set))
+
+koordinat = (10, 20, 10, 30, 20)
+print('Tuple:', koordinat)
+print('Set  :', set(koordinat))
+
+warna_set = {'merah', 'hijau', 'biru'}
+print('Set  :', warna_set)
+print('Tuple:', tuple(warna_set))
